8a44719f6497724c7870c9b197688470d87ce3e9
pontazaricardo <pontaza.ricardo@gmail.com>
2017-08-24 11:25:42 +0800

    Update

---
+++ main/dbms_gui_02/dbms_gui_02/dbms_objects_data/Table.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+
+namespace dbms_objects_data
+{
+    class Table
+    {
+        public DataTable table = new DataTable();
+
+        public Table()
+        {
+
+        }
+
+
+
+    }
+}
