70d199f08f71ef0d7871e3db222b4e8c7107e7b1
pontazaricardo <pontaza.ricardo@gmail.com>
2017-08-25 12:06:07 +0800

    Creation of DataTable object

--- main/dbms_gui_02/dbms_gui_02/dbms_objects_data/Database.cs
+++ main/dbms_gui_02/dbms_gui_02/dbms_objects_data/Database.cs
@@ -6,8 +6,53 @@ using System.Threading.Tasks;
 
 namespace dbms_objects_data
 {
-    public class Database
+    public sealed class Database
     {
+        public List<Table> tables;
 
+        private static readonly object obj = new object();
+        private static Database instance = null;
+
+        public static Database GetInstance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    lock (obj)
+                    {
+                        if(instance == null)
+                        {
+                            instance = new Database();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private Database()
+        {
+            tables = new List<Table>();
+        }
+
+
+        public bool AddTable(Table table)
+        {
+            if(table == null)
+            {
+                return false;
+            }
+            try
+            {
+                tables.Add(table);
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+        
     }
 }
