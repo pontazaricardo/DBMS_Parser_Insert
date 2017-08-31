8e1c082e2045603cd55e71fe140139a93a3926d1
pontazaricardo <pontaza.ricardo@gmail.com>
2017-08-31 11:26:00 +0800

    Update

--- main/dbms_gui_02/dbms_gui_02/dbms_gui_02/Program.cs
+++ main/dbms_gui_02/dbms_gui_02/dbms_gui_02/Program.cs
@@ -22,18 +22,18 @@ namespace dbms_gui_02
         /*
          * 
             
-            CREATE TABLE Student(studentId int, name varchar, gender varchar, age int);
-            CREATE TABLE Item itemID int, description varchar,);
-            CREATE TABLE Vehicle (licenseNumber varchar,brand varchar,model varchar,type varchar,engineSize int);
-            CREATE TABLE Course (courseName varchar,startingDate date,teacherName varchar);
-            CREATE TABLE Book (isbn varchar,title varchar,author varchar,pages int,editorial varchar);
-            INSERT INTO Student VALUES(10, 'John Smith', 'M', 22);
-            INSERT INTO Student VALUES(11, 'Hsu You-Ting', 'F', 23);
-            INSERT INTO Student(name, age, studentId, gender) VALUES('Ai Toshiko', 21, 12, 'F');
-            INSERT INTO Student(age, studentId, gender, name) VALUES(20, 13, 'M', 'Fernando Sierra');
-            INSERT INTO Student VALUES(14, 'Mohammed Ali', 'M', 25);
-            INSERT INTO Student VALUES(10, 'Huang Hao-Wei', 'M', 26);
-            INSERT INTO Book VALUES(12345, 'Romeo and Juliet', 'Shakespeare','Hello', 'Prentice Hall');
+CREATE TABLE Student(studentId int, name varchar, gender varchar, age int);
+CREATE TABLE Item itemID int, description varchar,);
+CREATE TABLE Vehicle (licenseNumber varchar,brand varchar,model varchar,type varchar,engineSize int);
+CREATE TABLE Course (courseName varchar,startingDate date,teacherName varchar);
+CREATE TABLE Book (isbn varchar,title varchar,author varchar,pages int,editorial varchar);
+INSERT INTO Student VALUES(10, 'John Smith', 'M', 22);
+INSERT INTO Student VALUES(11, 'Hsu You-Ting', 'F', 23);
+INSERT INTO Student(name, age, studentId, gender) VALUES('Ai Toshiko', 21, 12, 'F');
+INSERT INTO Student(age, studentId, gender, name) VALUES(20, 13, 'M', 'Fernando Sierra');
+INSERT INTO Student VALUES(14, 'Mohammed Ali', 'M', 25);
+INSERT INTO Student VALUES(10, 'Huang Hao-Wei', 'M', 26);
+INSERT INTO Book VALUES(12345, 'Romeo and Juliet', 'Shakespeare','Hello', 'Prentice Hall');
 
          */
     }
