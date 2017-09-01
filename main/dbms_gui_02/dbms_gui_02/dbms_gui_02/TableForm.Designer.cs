92737c39cb28e81c7c81b47a08fc0abe56540479
pontazaricardo <pontaza.ricardo@gmail.com>
2017-09-01 16:01:10 +0800

    Update

--- main/dbms_gui_02/dbms_gui_02/dbms_gui_02/TableForm.Designer.cs
+++ main/dbms_gui_02/dbms_gui_02/dbms_gui_02/TableForm.Designer.cs
@@ -28,11 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_tableName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Table name:";
+            // 
+            // lbl_tableName
+            // 
+            this.lbl_tableName.AutoSize = true;
+            this.lbl_tableName.Location = new System.Drawing.Point(88, 14);
+            this.lbl_tableName.Name = "lbl_tableName";
+            this.lbl_tableName.Size = new System.Drawing.Size(35, 13);
+            this.lbl_tableName.TabIndex = 1;
+            this.lbl_tableName.Text = "Name";
+            // 
+            // TableForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 421);
+            this.Controls.Add(this.lbl_tableName);
+            this.Controls.Add(this.label1);
+            this.Name = "TableForm";
             this.Text = "TableForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_tableName;
     }
 }
